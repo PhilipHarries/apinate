a420660436e5c3b18adc4ad2f9e4f59b17fa063f
Philip Harries <philip.harries.unix.consulting@gmail.com>
2016-09-06 23:07:45 +0100

    Added initial man pages

--- build.sh
+++ build.sh
@@ -19,9 +19,12 @@ build_dir=/tmp/fpm/apinate
 mkdir -p ${build_dir}/etc/apinate
 mkdir -p ${build_dir}/usr/share/apinate/templates
 mkdir -p ${build_dir}/usr/bin
+mkdir -p ${build_dir}/usr/share/man/man1
 
 cp ./templates/plain.tmpl ${build_dir}/usr/share/apinate/templates
 cp ${GOPATH}/bin/apinate ${build_dir}/usr/bin
+cp ./man/apinate.1 ${build_dir}/usr/share/man/man1
+gzip /usr/share/man/man1/apinate.1
 
 fpm -s dir -t rpm -n apinate -v $1 -C ${build_dir}
 fpm -s dir -t deb -n apinate -v $1 -C ${build_dir}
