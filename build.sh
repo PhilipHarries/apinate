83672af51b98f36a4c1b83397fa8306d6fee2b79
Philip Harries <philip.harries.unix.consulting@gmail.com>
2016-09-07 18:14:54 +0100

    Improved man pages and build script

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ gom build -o $GOPATH/bin/apinate
 
 build_dir=/tmp/fpm/apinate
 
+[[ -d ${build_dir} ]] && rm -rf ${build_dir}
 mkdir -p ${build_dir}/etc/apinate
 mkdir -p ${build_dir}/usr/share/apinate/templates
 mkdir -p ${build_dir}/usr/bin
@@ -24,7 +25,7 @@ mkdir -p ${build_dir}/usr/share/man/man1
 cp ./templates/plain.tmpl ${build_dir}/usr/share/apinate/templates
 cp ${GOPATH}/bin/apinate ${build_dir}/usr/bin
 cp ./man/apinate.1 ${build_dir}/usr/share/man/man1
-gzip /usr/share/man/man1/apinate.1
+gzip ${build_dir}/usr/share/man/man1/apinate.1
 
 fpm -s dir -t rpm -n apinate -v $1 -C ${build_dir}
 fpm -s dir -t deb -n apinate -v $1 -C ${build_dir}
